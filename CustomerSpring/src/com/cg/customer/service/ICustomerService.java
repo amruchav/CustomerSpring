49d4c47fde7fdce8dce6c6af4ecdbb7fbb2dd962
amruchav <32138342+amruchav@users.noreply.github.com>
2017-10-26 12:16:24 +0530

    Add files via upload

---
+++ CustomerSpring/src/com/cg/customer/service/ICustomerService.java
@@ -0,0 +1,20 @@
+package com.cg.customer.service;
+
+import java.util.List;
+
+import com.cg.customer.model.Customer;
+
+public interface ICustomerService {
+
+	void addDetails(Customer bean);
+
+	Customer retrieveById(int id);
+
+	List<Customer> retrieveDetails();
+
+	void deleteDetails(int custId);
+
+	List<Integer> retrieveAllIds();
+
+	
+}
